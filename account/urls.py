12c46019dcc0ae156ffa84002911e175dd3599a6
Kydyrbek97 <Kydyrali_097@mail.ru>
2023-01-18 21:12:21 +0600

    added login and category

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from account import views
+from account.views import LogoutView
+
 urlpatterns = [
     path('register/', views.RegistrationView.as_view()),
     path('activate/<uuid:activation_code>/', views.ActivationView.as_view()),
+    path('login/', views.LoginView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
+    path('logout/', LogoutView.as_view()),
 ]
